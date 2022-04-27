b716d028bf841d87a1fac3970781080a3fd8e69e
Leno4ek86 <102864231+Leno4ek86@users.noreply.github.com>
2022-04-27 10:45:34 +0300

    отформатирован код

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         int amount = 100;  // у клиента на счете
-        int contributed = 1550;  // сумма пополнения
+        int contributed = 1000;  // сумма пополнения
         int bonus = 100;  // бонус за пополнение
 
-        boolean reffil = true;
+        boolean isReffil = true;
 
-        if (reffil) {
+        if (isReffil) {
             int ruble = 1;
         } else {
-           int ruble = 0;
+            int ruble = 0;
         }
 
         System.out.println(contributed / bonus + "получено бонусов");
